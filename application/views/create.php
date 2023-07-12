ae1f7d68d97f25e205a292065dcfd96a9d6e5ccc
Lavish <aroralavishlavu@gmail.com>
2023-07-12 12:47:16 +0530

    delete the redudencies

--- application/views/create.php
+++ application/views/create.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>curd application -- create user</title>
+    <title>curd application--create user</title>
    <link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/css/bootstrap.min.css';?>">
 </head>
 <body>
